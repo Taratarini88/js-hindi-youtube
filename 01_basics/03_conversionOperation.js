e8fa1ca2a05f2b0b52049ef604cb6b4517d895e2
Taratarini88 <89439461+Taratarini88@users.noreply.github.com>
2023-10-01 11:01:53 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+// let score = 33
+// let score = "33"
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+// let score = false
+let score = "tara"
+
+
+// console.log (typeof score);
+// console.log (typeof (score));
+
+let valueInNumber = Number(score);
+// console.log (typeof valueInNumber);
+// console.log (valueInNumber);
+
+// "33" => 33 (string to integer conversion)
+// "33abc" = NaN (Not a Number but  33abc  datatype is a Number)
+// true => 1; false =>0 (Boolean type)
+
+
+// let isLoggedIn = 1
+let isLoggedIn = "Tara"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Tara" => trues
+
+
+let someName = 33
+
+let stringNumber = String(someName);
+console.log(stringNumber);
+console.log(typeof stringNumber);
