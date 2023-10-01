04d1b3b05d06ed37ace4e3a2eca3fb2aafca0a52
Taratarini88 <89439461+Taratarini88@users.noreply.github.com>
2023-10-01 06:22:00 +0000

    Datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+let name = "hitesh"
+let age = 3
+let isLoggedIn = false
+let state;
+// let state = null
+
+
+// number => 20 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object 
+
+// console.log(typeof age)
+
+// console.log(typeof null) // object
+
+// console.log(typeof undefined) // undefined
